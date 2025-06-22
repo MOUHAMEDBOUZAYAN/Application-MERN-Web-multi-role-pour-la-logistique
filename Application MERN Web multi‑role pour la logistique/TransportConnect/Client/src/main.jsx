1f64be4266d803a55e03e923423f857cbc7ada8b
mouhamedbouzayan <mohammedbouzi177@gmail.com>
2025-06-22 08:10:49 +0100

    Mettre à jour la structure HTML et le style CSS, ajouter des configurations Tailwind, intégrer le Toaster pour les notifications, et ajuster la configuration Vite.

--- Application MERN Web multi‑role pour la logistique/TransportConnect/Client/src/main.jsx
+++ Application MERN Web multi‑role pour la logistique/TransportConnect/Client/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './assets/styles/index.css'
+import { Toaster } from 'react-hot-toast'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+    <Toaster 
+      position="top-right"
+      toastOptions={{
+        duration: 4000,
+        style: {
+          background: '#363636',
+          color: '#fff',
+        },
+        success: {
+          duration: 3000,
+          theme: {
+            primary: 'green',
+            secondary: 'black',
+          },
+        },
+      }}
+    />
+  </React.StrictMode>,
 )
